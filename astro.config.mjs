d85d80d008ec29a7d8cce49659b2ad416ff0a01e
Oleksa Novyk <inavix@gmail.com>
2024-10-24 09:47:32 +0300

    simplify titles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,55 +1,50 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-      customCss: [
-        './src/styles/theme.css',
-      ],
-			title: 'База знань Bluesky',
+  integrations: [
+    starlight({
+      customCss: ['./src/styles/theme.css'],
+      title: 'База знань Bluesky',
       defaultLocale: 'root',
       locales: {
         root: {
           label: 'Українська',
-          lang: 'uk'
+          lang: 'uk',
         },
       },
       editLink: {
         baseUrl: 'https://github.com/navix/info.bsky.one/edit/main/',
       },
-			social: {
-				github: 'https://github.com/navix/info.bsky.one',
+      social: {
+        github: 'https://github.com/navix/info.bsky.one',
         blueSky: 'https://bsky.app/profile/bsky.one',
-			},
-			sidebar: [
+      },
+      sidebar: [
         {
           label: 'Home',
-          slug: 'home'
+          slug: 'home',
         },
         {
           label: 'Основи',
           items: [
-            { slug: 'basics/bluesky' },
-            { slug: 'basics/atproto' },
-            { slug: 'basics/join' },
-            { slug: 'basics/public', label: 'Що публічне і приватне?' },
-            { slug: 'basics/handle' },
-            { slug: 'basics/feed' },
-            { slug: 'basics/mute' },
-            { slug: 'basics/block' },
-          ]
+            {slug: 'basics/bluesky'},
+            {slug: 'basics/atproto'},
+            {slug: 'basics/join'},
+            {slug: 'basics/public'},
+            {slug: 'basics/handle'},
+            {slug: 'basics/feed'},
+            {slug: 'basics/mute'},
+            {slug: 'basics/block'},
+          ],
         },
-				{
-					label: 'Посібники',
-					items: [
-						{ slug: 'guides/custom-handle' },
-						{ slug: 'guides/custom-feed' },
-					],
-				},
-			],
-		}),
-	],
+        {
+          label: 'Посібники',
+          items: [{slug: 'guides/custom-handle'}, {slug: 'guides/custom-feed'}],
+        },
+      ],
+    }),
+  ],
 });
